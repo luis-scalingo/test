baf6f2ea5be8e6655f64296a04da24b88a3fab2f
Luis Pineda Morales <luis@lpi-sc-workstation.local>
2023-12-19 12:53:09 +0100

    modified index

--- index.php
+++ index.php
@@ -1,1 +1,35 @@
-<?php echo '<p> Hello World test </p>'; ?>
+<?php
+
+// Include the AWS SDK using the Composer autoloader.
+require 'vendor/autoload.php';
+
+use Aws\S3\S3Client;
+use Aws\S3\Exception\S3Exception;
+
+$bucket = 'myscalingobucket';
+$key = 'horse.png';
+
+echo '<h1> Hello World test </h1>';
+
+try {
+    //Create a S3Client
+    $s3Client = new S3Client([
+        'profile' => 'default',
+        'region' => 'eu-west-3',
+        'version' => '2006-03-01'
+    ]);
+
+    // Save object to a file.
+    $result = $s3Client->getObject(array(
+        'Bucket' => $bucket,
+        'Key' => $key,
+        'SaveAs' => $key
+    ));
+
+    echo "<img src='$key'>";
+
+} catch (S3Exception $e) {
+    echo $e->getMessage() . "\n";
+}
+
+?>
