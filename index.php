07b5ec40779ed5d4594c2a5623d897cadc0ee8cf
Luis Pineda Morales <luis@lpi-sc-workstation.local>
2023-12-19 13:01:16 +0100

    modif s3

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ require 'vendor/autoload.php';
 use Aws\S3\S3Client;
 use Aws\S3\Exception\S3Exception;
 
-$bucket = 'myscalingobucket';
+$mybucket = 'myscalingobucket';
 $key = 'horse.png';
 
 echo '<h1> Hello World test </h1>';
@@ -19,14 +19,24 @@ try {
         'version' => '2006-03-01'
     ]);
 
+    echo "<h3>Successful connection</h3>";
+
+    echo "<p>Bucket list</p>";
+
+    //Listing all S3 Bucket
+    $buckets = $s3Client->listBuckets();
+    foreach ($buckets['Buckets'] as $bucket) {
+        echo $bucket['Name'] . "\n";
+    }
+
     // Save object to a file.
     $result = $s3Client->getObject(array(
-        'Bucket' => $bucket,
+        'Bucket' => $mybucket,
         'Key' => $key,
         'SaveAs' => $key
     ));
 
-    echo "<img src='$key'>";
+    echo "<p><img src='$key'></p>";
 
 } catch (S3Exception $e) {
     echo $e->getMessage() . "\n";
