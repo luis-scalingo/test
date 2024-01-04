7274161749bd329279e73daea9362687f5b66341
Luis Pineda Morales <luis@lpi-sc-workstation.home>
2024-01-04 09:34:09 +0100

    aws test scalingo

--- index.php
+++ index.php
@@ -5,9 +5,10 @@ require 'vendor/autoload.php';
 
 use Aws\S3\S3Client;
 use Aws\S3\Exception\S3Exception;
+use Aws\Credentials\CredentialProvider;
 
-$mybucket = 'myscalingobucket';
-$key = 'horse.png';
+$mybucket = 'lpi-demo-bucket';
+$key = 'lake.png';
 
 echo '<h1> Hello World test </h1>';
 
@@ -17,31 +18,23 @@ try {
     if (!getenv("AWS_ACCESS_KEY_ID")) {
         //use credentials file
         $s3Client = new S3Client([
-            'profile' => 'default',
-            'region' => 'eu-west-3',
-            'version' => '2006-03-01'
+            'profile' => 'scalingo-key',
+            'region' => 'eu-central-1'
         ]);
     }else{
         //use env variables
         $s3Client = new S3Client([
-            'region' => 'eu-west-3',
+            'region' => 'eu-central-1',
             'version' => '2006-03-01',
-            'credentials' => [
-                'key' => getenv("AWS_ACCESS_KEY_ID"),
-                'secret'  => getenv("AWS_SECRET_ACCESS_KEY"),
-            ],
+            'credentials' => CredentialProvider::env()
+//            'credentials' => [
+//                'key' => getenv("AWS_ACCESS_KEY_ID"),
+//                'secret'  => getenv("AWS_SECRET_ACCESS_KEY"),
+//            ],
         ]);
     }
 
     echo "<h3>Successful connection</h3>";
-
-    echo "<p>Bucket list</p>";
-
-    //Listing all S3 Bucket
-    $buckets = $s3Client->listBuckets();
-    foreach ($buckets['Buckets'] as $bucket) {
-        echo $bucket['Name'] . "\n";
-    }
 
     // Save object to a file.
     $result = $s3Client->getObject(array(
